2912793f82ac4e7f7028be1ba917a6c9bb8992a6
LAPTOPRK-2021\René <rsh.kwee@gmail.com>
2022-08-24 21:28:14 +0200

    Eerste stapjes filtering

--- src/main/java/ofxLibrary/OfxFilter.java
+++ src/main/java/ofxLibrary/OfxFilter.java
@@ -10,4 +10,11 @@ public class OfxFilter {
   private String accountto = "";
   private String memo = "";
 
+  public boolean CheckTrans(OfxTransaction a_tran1, OfxTransaction a_tran2) {
+    boolean l_bstat = false;
+    l_bstat = (a_tran1.getAccount() == a_tran2.getAccountto()) && (a_tran1.getDtposted() == a_tran2.getDtposted());
+
+    return l_bstat;
+  }
+
 }
